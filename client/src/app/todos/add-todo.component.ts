77abef0baed3358c1c0d929eba6a0f60544f0d84
Robert-Beane <beane031@morris.umn.edu>
2019-10-06 14:46:32 -0500

    Rename input names Adjust validators

--- client/src/app/todos/add-todo.component.ts
+++ client/src/app/todos/add-todo.component.ts
@@ -42,7 +42,7 @@ export class AddTodoComponent implements OnInit {
     // add todo form validations
     this.addTodoForm = this.fb.group({
       // We allow alphanumeric input and limit the length for name.
-      name: new FormControl('name', Validators.compose([
+      owner: new FormControl('owner', Validators.compose([
         OwnerValidator.validOwner,
         Validators.minLength(2),
         Validators.maxLength(25),
@@ -51,19 +51,16 @@ export class AddTodoComponent implements OnInit {
       ])),
 
       // Since this is for a company, we need workers to be old enough to work, and probably not older than 200.
-      age: new FormControl('age', Validators.compose([
-        Validators.pattern('^[0-9]+[0-9]?'),
-        Validators.min(15),
-        Validators.max(200),
+      status: new FormControl('status', Validators.compose([
         Validators.required
       ])),
 
       // We don't care much about what is in the company field, so we just add it here as part of the form
       // without any particular validation.
-      company: new FormControl('company'),
+      category: new FormControl('category'),
 
       // We don't need a special validator just for our app here, but there is a default one for email.
-      email: new FormControl('email', Validators.email)
+      body: new FormControl('body')
     })
 
   }

c4b627b825fad8d9fa703543dc500b3dcc4f4b93
Robert-Beane <beane031@morris.umn.edu>
2019-10-06 19:07:18 -0500

    Add tests for todo.component

--- client/src/app/todos/todo.component.spec.ts
+++ client/src/app/todos/todo.component.spec.ts
@@ -1,25 +1,71 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, async} from '@angular/core/testing';
+import {Todo} from './todo';
+import {TodoComponent} from './todo.component';
+import {TodoListService} from './todo-list.service';
+import {Observable} from 'rxjs/Observable';
+import {CustomModule} from "../custom.module";
 
-import { TodoComponent } from './todo.component';
+describe('Todo component', () => {
 
-// describe('TodoComponent', () => {
-//   let component: TodoComponent;
-//   let fixture: ComponentFixture<TodoComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ TodoComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(TodoComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  let todoComponent: TodoComponent;
+  let fixture: ComponentFixture<TodoComponent>;
+
+  let todoListServiceStub: {
+    getTodoById: (todoId: string) => Observable<Todo>
+  };
+
+  beforeEach(() => {
+    // stub TodoService for test purposes
+    todoListServiceStub = {
+      getTodoById: (todoId: string) => Observable.of([
+        {
+          _id: 'java_id',
+          owner: 'Java',
+          status: false,
+          category: 'Code',
+          body: 'Java is very fun to learn'
+        },
+        {
+          _id: 'python_id',
+          owner: 'Python',
+          status: false,
+          category: 'Snake',
+          body: 'snakes are not nice'
+        },
+        {
+          _id: 'cee_id',
+          owner: 'Cee',
+          status: true,
+          category: 'Letter',
+          body: 'this is an a and b conversation, cee your way out of it.'
+        }
+      ].find(todo => todo._id === todoId))
+    };
+
+    TestBed.configureTestingModule({
+      imports: [CustomModule],
+      declarations: [TodoComponent],
+      providers: [{provide: TodoListService, useValue: todoListServiceStub}]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TodoComponent);
+      todoComponent = fixture.componentInstance;
+    });
+  }));
+
+  it('can retrieve python by ID', () => {
+    todoComponent.setId('python_id');
+    expect(todoComponent.todo).toBeDefined();
+    expect(todoComponent.todo.owner).toBe('Python');
+    expect(todoComponent.todo.body).toBe('snakes are not nice');
+  });
+
+  it('returns undefined for Rudolph', () => {
+    todoComponent.setId('Rudolph');
+    expect(todoComponent.todo).not.toBeDefined();
+  });
+
+});
